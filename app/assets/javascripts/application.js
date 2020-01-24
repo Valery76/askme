ff816426522bbb7be61116ffcbb90d321b90ee74
Valery <walerka100@yandex.ru>
2020-01-24 15:42:48 +0300

    redesigned user statistics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,9 +19,11 @@
 // Этот код добавляет обработчик события выдвигания формы при клике на кнопку
 // «Задать вопрос», которая видна только, когда страница открыта в маленьком
 // окне.
-$(function(){
-  $('#ask-button').click(function(){
-    $('#ask-form').slideToggle(300);
-    return false;
+$(document).on('turbolinks:load', function() {
+  $(function(){
+    $('#ask-button').click(function(){
+      $('#ask-form').slideToggle(300);
+      return false;
+    });
   });
 });
